94f29c24b357f654e0171de91dd629c92ae295b4
BuildTools <unconfigured@null.spigotmc.org>
2026-03-17 00:48:37 +0200

    https fix

--- client/app/forgot-password/page.tsx
+++ client/app/forgot-password/page.tsx
@@ -14,7 +14,7 @@ export default function ForgotPassword() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://retro-audio-shop.vercel.app/auth/forgot-password", {
+      const res = await fetch("https://retro-audio-shop.vercel.app/auth/forgot-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email })
