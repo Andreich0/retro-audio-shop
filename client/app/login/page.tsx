f7d811626575f017d24d9287a98617cc25e8f8c5
BuildTools <unconfigured@null.spigotmc.org>
2026-03-26 00:29:09 +0200

    fix: total project re-index to kill ghost routes

---
+++ client/app/login/page.tsx
@@ -0,0 +1,4 @@
+import { NextResponse } from 'next/server';
+export async function GET() {
+  return NextResponse.redirect(new URL('/auth?mode=login', process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'));
+}
